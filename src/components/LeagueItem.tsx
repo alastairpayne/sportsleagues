7badc2bbd1b03291c28821c6aeed4b252abb24d0
Alastair Payne <alastairpayne@gmail.com>
2025-09-24 00:42:00 +0100

    Add: Basic styling to indicate element clickable

--- src/components/LeagueItem.tsx
+++ src/components/LeagueItem.tsx
@@ -1,5 +1,8 @@
 import React from 'react';
 
+//TODO - move each component into a folder if it'll have multiple files
+import './LeagueItem.css';
+
 type LeagueItemProps = {
 	idLeague: string,
 	strLeague: string,
@@ -14,7 +17,7 @@ export const LeagueItem: React.FC<LeagueItemProps> = ({
   strLeagueAlternate,
 }) => {
   return (
-    <div>
+    <div className="league-item">
       <h2>{strLeague}</h2>
       <p>{strSport}</p>	
       <p>{strLeagueAlternate}</p>
